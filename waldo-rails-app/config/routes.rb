0160fe1cd3247ed0b352077ac2fb7b5ea1faafc3
jbk2 <james@humanbenefits.com>
2025-05-29 09:32:10 +0200

    generate & migrate rails auth

--- waldo-rails-app/config/routes.rb
+++ waldo-rails-app/config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
 
-  get "characters/show"
-  get "characters/index"
-  get "characters/update"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
