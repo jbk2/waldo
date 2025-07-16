e7652a66e7c5bed1e8cc81bb3e7896a967a79b38
jbk2 <james@humanbenefits.com>
2025-07-16 17:41:32 +0200

    Minor stylling in EndGameDialog.jsx

--- waldo-react-app/src/components/EndGameDialog.jsx
+++ waldo-react-app/src/components/EndGameDialog.jsx
@@ -22,12 +22,13 @@ export default function EndGameDialog() {
       className="fixed top-2/5 left-1/2 -translate-x-1/2 w-90 h-44 opacity-95
         bg-white border-2 rounded-md justify-center items-center p-4">
       <p>
-        Well done, you found all the characters, in a time of –&nbsp;
+        Well done, you found all the characters.<br/>
+        It took you –&nbsp;
         <span className="text-md font-mono font-medium tabular-nums lining-nums underline
-          decoration-wavy decoration-1 decoration-green-600 underline-offset-3">
+          decoration-wavy decoration-1 decoration-green-600 underline-offset-3 pr-[1px]">
           { gameCompletedLengthSeconds }
         </span>
-        s
+        s.
       </p>
       <button onClick={handleStartGameClick} className="block mx-auto mt-5 h-fit p-3 px-4
         rounded-lg hover:cursor-pointer border-2 border-indigo-300 hover:border-indigo-400
