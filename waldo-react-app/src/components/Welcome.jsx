767f743ec9fc73284a845b89924c310ce3cab1be
jbk2 <james@humanbenefits.com>
2025-08-18 14:44:11 +0200

    Further final Welcome content & styling

--- waldo-react-app/src/components/Welcome.jsx
+++ waldo-react-app/src/components/Welcome.jsx
@@ -1,9 +1,8 @@
-import daisyUiLogo from '../assets/images/logos/daisyui-logotype.svg';
-import htmlLogo from '../assets/images/logos/html5-logo.svg';
+import daisyUiLogo from '../assets/images/logos/daisyui-logomark.svg';
 import postgresLogo from '../assets/images/logos/postgres-logo.svg';
 import railsLogo from '../assets/images/logos/rails-logo.svg';
 import rspecLogo from '../assets/images/logos/rspec-logo.svg';
-import tailwindcssLogo from '../assets/images/logos/tailwindcss-logotype.svg';
+import tailwindcssLogo from '../assets/images/logos/tailwindcss-logomark.svg';
 import viteLogo from '../assets/images/logos/vite-logo.svg';
 import reactLogo from '../assets/images/logos/react-logo.svg';
 import reactRouterLogo from '../assets/images/logos/react-router-logo.svg';
@@ -12,7 +11,7 @@ export default function Welcome({setViewedWelcome}) {
 
   
   return(
-    <div className='mt-[4vh] md:mt-[7vh] xl:mt-[10vh] mx-4'>
+    <div className=' mt-[4vh] md:mt-[7vh] xl:mt-[9vh] mx-4'>
       <div id="intro-text" className='max-w-130 lg:max-w-230 mx-auto'>
         <p className="inline text-md text-gray-600">A toy Where's Waldo app, built by</p>
         <a href="https://github.com/jbk2/fakebook/blob/main/README.md" className="inline link link-primary text-sm">&nbsp;James Kemp</a>
@@ -22,7 +21,7 @@ export default function Welcome({setViewedWelcome}) {
           <a href="https://github.com/jbk2/fakebook/blob/main/README.md" className="inline link link-primary text-sm">Detailed readme</a>
         </div>
       </div>
-      <div className='flex flex-col lg:flex-row justify-center items-center lg:items-start gap-10 mx-4 mt-10'>
+      <div className='flex flex-col lg:flex-row justify-center items-center lg:items-start gap-7 lg:gap-10 mx-4 mt-5 lg:mt-10'>
         <section id="rails" className='max-w-140'>
           <div id='section-header' className='flex gap-10 mb-3.5'>
             <div className='flex w-25'>
@@ -64,25 +63,30 @@ export default function Welcome({setViewedWelcome}) {
           </div>
           <div className='grid grid-cols-[24px_auto] gap-2 text-sm text-gray-800'>
             <p className=''>🪝</p>
-            <p className=''>Hooks; all the usual, custom useReducers.</p>
+            <p className=''>Hooks; state, context, effect, callback, memo, ref, navigate, params.</p>
             <img src={reactRouterLogo} alt="react router logo" className='w-[16px] inline mt-[5px]'/>
-            <p className=''>React Router; fully routed, declaratively, sprinkles of data mode.</p>
+            <p className=''>React Router; fully routed, declaratively, some data mode.</p>
             <p className=''>📦</p>
             <p className=''>Heavy use of Context API for housing state.</p>
             <p className=''>🛠️</p>
-            <p className=''>Utils - fetch from Rails API code held within servie utilities.</p>
+            <p className=''>Utils - fetch, from Rails API, code held within service utilities.</p>
             <p className=''>✅</p>
-            {/* <img src={viteLogo} alt="vite logo" className='w-[16px] inline mt-[5px]'/> */}
             <p className=''>Full mocked component unit testing with Vite.</p>
             <p className=''>🔗</p>
-            <p className=''>Core functionality integration tested, full test server setup</p>
+            <p className=''>Core functionality integration tested, full test server setup.</p>
+            <div className='flex'>
+              <img src={tailwindcssLogo} alt="tailwind logo" className='w-[18px] inline'/>
+              <img src={daisyUiLogo} alt="daisyui logo" className='w-[14px] inline ml-1'/>
+            </div>
+            <p className='inline ml-3'>Tailwind CSS, with some DaisyUI component library, styled.</p>
           </div>
         </section>
       </div>
-
-      <button className='btn btn-primary block mx-auto mt-10' onClick={() => setViewedWelcome(true)}>
+      
+      <button className='btn btn-primary block mx-auto mt-6 lg:mt-12' onClick={() => setViewedWelcome(true)}>
         Proceed to app
       </button>
+    
     </div>    
   )
 }
